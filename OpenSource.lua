d1afab3f87c81a08395b134f928f1b5d73e5f6c5
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-03-13 16:53:20 +0900

    Update OpenSource.lua

--- OpenSource.lua
+++ OpenSource.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/HexaGlove.luau'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/Gloves/HexaGlove.luau'))()
